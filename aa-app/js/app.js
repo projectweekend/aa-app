468bb29b73c2be831ba543a32f5aed683646220d
Brian Hines <brian@projectweekend.net>
2018-04-08 19:00:21 -0500

    Get some data in the table

--- aa-app/js/app.js
+++ aa-app/js/app.js
@@ -1,16 +1,28 @@
+Vue.use('')
+
 var app = new Vue({
   el: '#app',
   data: {
-    greeting: 'Welcome to your Vue.js app!',
-    docsURL: 'http://vuejs.org/guide/',
-    discordURL: 'https://chat.vuejs.org',
-    forumURL: 'http://forum.vuejs.org/'
+    apiRoot: 'http://127.0.0.1:8000',
+    unitInfo: null
+  },
+  mounted: function () {
+    this.getUnitInfo();
   },
   methods: {
     humanizeURL: function (url) {
       return url
         .replace(/^https?:\/\//, '')
         .replace(/\/$/, '')
+    },
+    getUnitInfo: function () {
+      var url = this.apiRoot + '/unit-info'
+      // console.log(Vue);
+      this.$http.get(url).then(res => {
+        this.unitInfo = res.body;
+      }, res => {
+        console.log(res);
+      })
     }
   }
-})
+});
