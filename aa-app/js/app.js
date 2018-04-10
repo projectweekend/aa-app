c540a8c26b000c2c45eeda48b4770384d5e8be84
Brian Hines <brian@projectweekend.net>
2018-04-09 21:04:57 -0500

    Rough draft of battle info page

--- aa-app/js/app.js
+++ aa-app/js/app.js
@@ -4,25 +4,44 @@ var app = new Vue({
   el: '#app',
   data: {
     apiRoot: 'http://127.0.0.1:8000',
-    unitInfo: null
+    unitTypes: ['land', 'air', 'sea'],
+    unitInfo: null,
+    battle: {
+      attacker: {},
+      defender: {}
+    },
+    result: {}
   },
   mounted: function () {
     this.getUnitInfo();
   },
   methods: {
-    humanizeURL: function (url) {
-      return url
-        .replace(/^https?:\/\//, '')
-        .replace(/\/$/, '')
-    },
     getUnitInfo: function () {
       var url = this.apiRoot + '/unit-info'
       // console.log(Vue);
       this.$http.get(url).then(res => {
         this.unitInfo = res.body;
-      }, res => {
-        console.log(res);
+        this.initEmptyBattle();
+      }, err => {
+        console.log(err);
       })
+    },
+    initEmptyBattle: function () {
+      for (let t of this.unitTypes) {
+        for (let unit of this.unitInfo[t]) {
+          this.battle.attacker[unit.name] = 0;
+          this.battle.defender[unit.name] = 0;
+        }
+      }
+    },
+    simulateBattle: function () {
+      var url = this.apiRoot;
+      console.log(this.battle);
+      this.$http.post(url, this.battle).then(res => {
+        this.result = res.body;
+      }, err => {
+        console.log(err);
+      });
     }
   }
 });
