5de25a2e6f76790fa1c7ae8ba9e13c1361aeb22a
Brian Hines <brian@projectweekend.net>
2018-04-14 10:07:22 -0500

    Set up cloudfront and dns records

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,61 @@ resource "aws_s3_bucket" "bucket" {
 POLICY
 
   website {
-    index_document = "index.html"
+    index_document = "${var.index_page}"
     error_document = "error.html"
+  }
+}
+
+resource "aws_cloudfront_distribution" "cloudfront" {
+  origin {
+    domain_name = "${aws_s3_bucket.bucket.bucket_domain_name}"
+    origin_id   = "${var.origin_id}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "${var.index_page}"
+  aliases             = ["aa.exitcodezero.io"]
+  http_version        = "http2"
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.origin_id}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    acm_certificate_arn = "${var.certificate_arn}"
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+}
+
+resource "aws_route53_record" "cloudfront_dns" {
+  zone_id = "Z2NIHEK8Z6RTTA"
+  name    = "${var.bucket_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.cloudfront.domain_name}"
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
   }
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,15 @@
 variable "bucket_name" {
   default = "aa.exitcodezero.io"
 }
+
+variable "certificate_arn" {
+  default = "arn:aws:acm:us-east-1:421713074241:certificate/69e1c73c-d0bf-49e7-b187-8d0a8ec4dcc7"
+}
+
+variable "index_page" {
+  default = "index.html"
+}
+
+variable "origin_id" {
+  default = "S3-aa.exitcodezero.io-0"
+}
