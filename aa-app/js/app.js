ff409f9c204a74e8538d8f19bbb6057971c08b67
Brian Hines <brian@projectweekend.net>
2018-04-21 08:21:59 -0500

    Start work on land battle

--- aa-app/js/app.js
+++ aa-app/js/app.js
@@ -6,33 +6,63 @@ var app = new Vue({
     apiRoot: 'https://aa-api.exitcodezero.io',
     unitTypes: ['land', 'air', 'sea'],
     unitInfo: null,
+    amphibiousAssaultNavalUnits: [],
+    landBattleUnits: [],
+    navalBattleUnits: [],
     battle: {
       attacker: {},
-      defender: {}
+      defender: {},
+      amphibiousAssault: false
     },
     result: null
   },
   mounted: function () {
-    this.getUnitInfo();
+    this.GetUnitInfo();
+    this.EmptyBattle();
   },
   methods: {
-    getUnitInfo: function () {
-      var url = this.apiRoot + '/unit-info'
-      // console.log(Vue);
+    GetUnitInfo: function () {
+      var url = this.apiRoot + '/unit-info';
       this.$http.get(url).then(res => {
         this.unitInfo = res.body;
-        this.emptyBattle();
+        this.RefreshLandBattleUnits();
+        this.RefreshNavalBattleUnits();
+        this.RefreshAmphibiousAssaultNavalUnits();
       }, err => {
         console.log(err);
       })
     },
-    emptyBattle: function () {
-      for (let t of this.unitTypes) {
-        for (let unit of this.unitInfo[t]) {
-          this.battle.attacker[unit.name] = 0;
-          this.battle.defender[unit.name] = 0;
+    RefreshAmphibiousAssaultNavalUnits: function () {
+      this.amphibiousAssaultNavalUnits = [];
+    },
+    RefreshLandBattleUnits: function () {
+      this.landBattleUnits = [];
+      for (var i = 0; i < this.unitInfo.land.length; i++) {
+        this.landBattleUnits.push(this.unitInfo.land[i]);
+      }
+      for (var i = 0; i < this.unitInfo.air.length; i++) {
+        this.landBattleUnits.push(this.unitInfo.air[i]);
+      }
+    },
+    RefreshNavalBattleUnits: function () {
+      this.navalBattleUnits = [];
+      for (var i = 0; i < this.unitInfo.air.length; i++) {
+        this.navalBattleUnits.push(this.unitInfo.air[i]);
+      }
+      for (var i = 0; i < this.unitInfo.sea.length; i++) {
+        this.navalBattleUnits.push(this.unitInfo.sea[i]);
+      }
+    },
+    EmptyBattle: function () {
+      if (this.unitInfo) {
+        for (let t of this.unitTypes) {
+          for (let unit of this.unitInfo[t]) {
+            this.battle.attacker[unit.name] = 0;
+            this.battle.defender[unit.name] = 0;
+          }
         }
       }
+      this.amphibiousAssault = false;
     },
     emptyResult: function () {
       this.result = null;
@@ -45,6 +75,9 @@ var app = new Vue({
       }, err => {
         console.log(err);
       });
+    },
+    toggleAmphibiousAssault: function () {
+      this.battle.amphibiousAssault = !this.battle.amphibiousAssault;
     }
   }
 });
