f9382c7096c0144758bdd48d31b3bc483c1a2da1
Brian Hines <brian@projectweekend.net>
2018-04-23 21:38:29 -0500

    Land battle

--- aa-app/js/app.js
+++ aa-app/js/app.js
@@ -1,83 +1,45 @@
-Vue.use('')
-
 var app = new Vue({
   el: '#app',
   data: {
     apiRoot: 'https://aa-api.exitcodezero.io',
-    unitTypes: ['land', 'air', 'sea'],
-    unitInfo: null,
-    amphibiousAssaultNavalUnits: [],
-    landBattleUnits: [],
-    navalBattleUnits: [],
-    battle: {
-      attacker: {},
-      defender: {},
-      amphibiousAssault: false
-    },
+    landBattle: null,
+    landBattleResult: null,
     result: null
   },
   mounted: function () {
-    this.GetUnitInfo();
-    this.EmptyBattle();
+    this.GetLandBattleTemplate();
+  },
+  computed: {
+    attackerWinPercentage: function () {
+      var p = (this.landBattleResult.wins.attacker / this.landBattleResult.wins.total_played) * 100;
+      return Math.round(p * 100) / 100;
+    },
+    defenderWinPercentage: function () {
+      var p = (this.landBattleResult.wins.defender / this.landBattleResult.wins.total_played) * 100;
+      return Math.round(p * 100) / 100;
+    },
+    drawPercentage: function () {
+      var p = (this.landBattleResult.wins.draw / this.landBattleResult.wins.total_played) * 100;
+      return Math.round(p * 100) / 100;
+    }
   },
   methods: {
-    GetUnitInfo: function () {
-      var url = this.apiRoot + '/unit-info';
+    GetLandBattleTemplate: function () {
+      var url = this.apiRoot + '/land-battle';
       this.$http.get(url).then(res => {
-        this.unitInfo = res.body;
-        this.RefreshLandBattleUnits();
-        this.RefreshNavalBattleUnits();
-        this.RefreshAmphibiousAssaultNavalUnits();
+        this.landBattle = res.body;
       }, err => {
         console.log(err);
       })
     },
-    RefreshAmphibiousAssaultNavalUnits: function () {
-      this.amphibiousAssaultNavalUnits = [];
-    },
-    RefreshLandBattleUnits: function () {
-      this.landBattleUnits = [];
-      for (var i = 0; i < this.unitInfo.Land.length; i++) {
-        this.landBattleUnits.push(this.unitInfo.Land[i]);
-      }
-      for (var i = 0; i < this.unitInfo.Air.length; i++) {
-        this.landBattleUnits.push(this.unitInfo.Air[i]);
-      }
-    },
-    RefreshNavalBattleUnits: function () {
-      this.navalBattleUnits = [];
-      for (var i = 0; i < this.unitInfo.Air.length; i++) {
-        this.navalBattleUnits.push(this.unitInfo.Air[i]);
-      }
-      for (var i = 0; i < this.unitInfo.Sea.length; i++) {
-        this.navalBattleUnits.push(this.unitInfo.Sea[i]);
-      }
-    },
-    EmptyBattle: function () {
-      if (this.unitInfo) {
-        for (let t of this.unitTypes) {
-          for (let unit of this.unitInfo[t]) {
-            this.battle.attacker[unit.name] = 0;
-            this.battle.defender[unit.name] = 0;
-          }
-        }
-      }
-      this.amphibiousAssault = false;
-    },
-    emptyResult: function () {
-      this.result = null;
-    },
-    simulateBattle: function () {
-      this.emptyResult();
-      var url = this.apiRoot;
-      this.$http.post(url, this.battle).then(res => {
-        this.result = res.body;
+    SimulateLandBattle: function () {
+      var url = this.apiRoot + '/land-battle';
+      this.$http.post(url, this.landBattle).then(res => {
+        this.landBattleResult = res.body;
+        console.log(this.landBattleResult);
       }, err => {
         console.log(err);
       });
-    },
-    toggleAmphibiousAssault: function () {
-      this.battle.amphibiousAssault = !this.battle.amphibiousAssault;
     }
   }
 });
